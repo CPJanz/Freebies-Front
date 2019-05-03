332ef4d14e12054b2435f4fc48f1781974a62eb5
Carl Janz <carl.janz@gmail.com>
2019-05-02 23:03:16 -0700

    Updated findScreen to use db and device location to populate cards

---
+++ utils/API.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export default {
+  getNearbyItems: location => {
+    console.log("locatio", location);
+    const queryString =
+      "https://freebies-api.herokuapp.com/api/item?latitude=" +
+      location.latitude +
+      "&longitude=" +
+      location.longitude;
+    return axios.get(queryString);
+  }
+};

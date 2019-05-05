2e7d7798d0a8bf86f25795f4bf25ed85286fb382
Stacy Nowak <nowak.stacy@gmail.com>
2019-05-04 22:12:12 -0700

    added a function for each route to the back end

--- utils/API.js
+++ utils/API.js
@@ -13,10 +13,34 @@ export default {
   postNewItem: itemObject => {
     const queryString = "https://freebies-api.herokuapp.com/api/item";
     return axios.post(queryString, itemObject);
-  }
+  },
   // route for updating item as taken
+  itemTaken: itemId => {
+    const queryString = `https://freebies-api.herokuapp.com/api/item/${itemId}`;
+    return axios.put(queryString);
+  },
+
   // route for reposting an item
+  itemRepost: itemId => {
+    const queryString = `https://freebies-api.herokuapp.com/api/item/repost/${itemId}`;
+    return axios.put(queryString);
+  },
+
   // route to post a new user
+  newUser: email => {
+    const queryString = "https://freebies-api.herokuapp.com/api/user";
+    return axios.post(queryString, email);
+  },
+
   // route to get user information
+  findUser: userId => {
+    const queryString = `https://freebies-api.herokuapp.com/api/user/${userId}`;
+    return axios.get(queryString);
+  },
+
   // route to get a user's posted items
+  getPostedItems: userId => {
+    const queryString = `https://freebies-api.herokuapp.com/api/user/posts/${userId}`;
+    return axios.get(queryString);
+  }
 };

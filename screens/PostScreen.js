853c535d69e6e659cfd29e520db70230393e554c
Stacy Nowak <nowak.stacy@gmail.com>
2019-05-10 12:22:11 -0700

    toast text to yellow & center, remove okay btn

--- screens/PostScreen.js
+++ screens/PostScreen.js
@@ -109,7 +109,7 @@ export default class PostScreen extends Component {
         } else {
             Toast.show({
                 text: "Your post must include an image",
-                buttonText: "Okay",
+                textStyle: { color: "#f3d34a", textAlign: "center" },
                 duration: 3000,
                 position: "top"
               });

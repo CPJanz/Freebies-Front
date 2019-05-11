06aa596e7a664d19a60aa00627f7a55205e024f0
CGibeau <claire.gibeau@outlook.com>
2019-05-10 20:07:14 -0700

    update about screen

--- screens/AboutScreen.js
+++ screens/AboutScreen.js
@@ -34,26 +34,26 @@ export default class AboutScreen extends React.Component {
               <Text >
               Freebeez is an app designed for giving and getting free stuff without having to talk to anyone! Remember the days when you could put unwanted items on the corner with a free sign and first-come-first-serve it would be taken by some lucky person who happened by? Freebeez takes this to the next level by providing a platform for posting and finding free items.
               </Text>
-              <Text style={{fontWeight:"bold", marginTop:5, marginBottom:5}}>
+              <Text style={{fontWeight:"bold", marginTop:15, marginBottom:5}}>
               How Does It Work?
               </Text>
               <Text>
-              If you are searching for free stuff, simply sign up with your email address, and allow geolocation permissions. The app will do the rest by showing you a list of free items closest to you. Have some stuff you want to give away? Sign up with your email, allow geolocation permissions, and snap a photo or two of your stuff. The app will do the rest. Check it out! 
+              If you are searching for free stuff, simply sign up with your email address, and allow geolocation permissions. The app will do the rest by showing you a list of free items closest to you. Have some stuff you want to give away? Sign up with your email, allow geolocation permissions, and snap a photo or two of your stuff. The app will do the rest. Your items will be posted for 24 hours. If someone hasn't taken it in that time, you can repost. Check it out! 
               </Text>
             </Body>
           </CardItem>
-          <CardItem footer bordered>
-          <Text style={{fontWeight:"bold"}}>FAQs</Text>
-          </CardItem>
           <CardItem>
             <Body>
-              <Text style={{margin:5}}>
+              <Text style={{fontWeight:"bold", marginTop:5, marginBottom:5}}>
+              FAQs
+              </Text>
+              <Text>
               To report any concerns, Email us at <Text style={{color:"blue", textDecorationLine:"underline"}}>freebeez@freestuff.com</Text>.
               </Text>
-              <Text style={{margin:5}}>
+              <Text>
               No longer want to get or give free stuff? We are sad to see you go. Click <Text style={{color:"blue", textDecorationLine:"underline"}}>here</Text> and we’ll remove your account.
               </Text>
-              <Text style={{margin:5}}>
+              <Text>
               See more here: <Icon name="logo-github" onPress={() => Linking.openURL("https://github.com/CPJanz/Freebies-Front")} />
               </Text>
             </Body>

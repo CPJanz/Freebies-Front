acaa21ed6cae84337102dab063eef798e5727989
Stacy Nowak <nowak.stacy@gmail.com>
2019-05-14 21:15:21 -0700

    post item title & toast added & style moved to separate file

--- screens/PostScreen/index.js
+++ screens/PostScreen/index.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 import { AsyncStorage, KeyboardAvoidingView } from "react-native";
-import ImagePickerComponent from "../components/Camera";
+import ImagePickerComponent from "../../components/Camera";
 import { Container, Content, Text, Button, View, Form, Item, Input, Toast } from "native-base";
 import * as firebase from "firebase";
 import uuid from "uuid";
-import API from "../utils/API";
+import API from "../../utils/API";
+import styles from "./style";
 
 export default class PostScreen extends Component {
     images = [];
@@ -56,7 +57,7 @@ export default class PostScreen extends Component {
             xhr.open("GET", uri, true);
             xhr.send(null);
         });
-        
+
         // creates a reference based off of the generated image ID
         var ref = firebase
             .storage()
@@ -105,91 +106,62 @@ export default class PostScreen extends Component {
                 },
                 description: this.state.description
             });
-            
+            Toast.show({
+                text: "Your item was posted!",
+                textStyle: { color: "#f3d34a", textAlign: "center" },
+                duration: 3000,
+                position: "top"
+            });
         } else {
             Toast.show({
                 text: "Your post must include an image",
                 textStyle: { color: "#f3d34a", textAlign: "center" },
                 duration: 3000,
                 position: "top"
-              });
+            });
             this.setState({ makingPost: false });
         }
     };
 
     render() {
         return (
-            
-        <Container 
-            contentContainerStyle={{
-                backgroundColor: "#c2dfe3",
-            }}>
-            
-        <Content 
-            contentContainerStyle={{
-                flexGrow: 1,
-                justifyContent: "center",
-                backgroundColor: "#c2dfe3",
-            }}>
-            <KeyboardAvoidingView>
-        <Form style={{
-            backgroundColor: "#c2dfe3",
-            justifyContent: "center",
-            alignItems: "center"
-            }}>
-            <View style={{
-            paddingTop: 30,
-            paddingBottom: 30,
-            backgroundColor: "white",
-            width: "85%"
-            }}>
-            <Item style={{ borderColor: "transparent" }}>
-                <ImagePickerComponent images={this.images} />
-            </Item>
-            <Item style={{ borderColor: "transparent" }}>
-                <Input
-                    style={{ 
-                        margin: 15,
-                        padding: 20,
-                        borderLeftWidth: 1,
-                        borderTopWidth: 1,
-                        borderRightWidth: 1,
-                        borderBottomWidth: 1
-                    }}
-                    multiline={true}
-                    maxLength={50}
-                    placeholder="Optional item description"
-                    value={this.state.description}
-                    onChange={this.handleDiscriptionChange}
-                />
-            </Item>
-            <Item style={{
-                borderColor: "transparent",
-                flexDirection: "row",
-                justifyContent: "center",
-                padding: 10
-                }}>
-                <Button
-                    onPress={this.state.makingPost === false ? this.disableBtn : null}
-                    style={{
-                        marginRight: 20,
-                        backgroundColor: "#f3d34a"
-                    }}>
-                    <Text> Post </Text>
-                </Button>
-                <Button
-                    onPress={() => this.props.navigation.navigate("Give")}
-                    style={{
-                        backgroundColor: "#f3d34a"
-                    }}>
-                    <Text>Cancel</Text>
-                </Button>
-            </Item>
-            </View>
-        </Form>
-        </KeyboardAvoidingView>
-        </Content>
-        </Container>
+            <Container contentContainerStyle={styles.container}>
+                <Content contentContainerStyle={styles.content}>
+                    <KeyboardAvoidingView>
+                        <Form style={styles.form}>
+                            <Text style={styles.title}>Post Your Item!</Text>
+                            <View style={styles.whiteBox}>
+                                <Item style={styles.item}>
+                                    <ImagePickerComponent images={this.images} />
+                                </Item>
+                                <Item style={styles.item}>
+                                    <Input style={styles.input}
+                                        multiline={true}
+                                        maxLength={50}
+                                        placeholder="Optional item description"
+                                        value={this.state.description}
+                                        onChange={this.handleDiscriptionChange}
+                                    />
+                                </Item>
+                                <Item style={styles.btnBox}>
+                                    <Button
+                                        onPress={this.state.makingPost === false ? this.disableBtn : null}
+                                        style={styles.postBtn}
+                                    >
+                                        <Text> Post </Text>
+                                    </Button>
+                                    <Button
+                                        onPress={() => this.props.navigation.navigate("Give")}
+                                        style={styles.cancelBtn}
+                                    >
+                                        <Text>Cancel</Text>
+                                    </Button>
+                                </Item>
+                            </View>
+                        </Form>
+                    </KeyboardAvoidingView>
+                </Content>
+            </Container>
         )
     }
 };

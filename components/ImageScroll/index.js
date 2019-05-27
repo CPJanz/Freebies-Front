87ff36417ff239f19e29d2758e87d8a6415bcc08
Carl Janz <carl.janz@gmail.com>
2019-05-27 12:43:10 -0700

    Finished implementing preview images and removed bee temporary image

--- components/ImageScroll/index.js
+++ components/ImageScroll/index.js
@@ -2,33 +2,30 @@ import React from "react";
 import { ScrollView, View } from "react-native";
 import PlaceholderImage from "../PlaceholderImage";
 import { Image } from "react-native-expo-image-cache";
+import { Dimensions } from "react-native";
+const imageWidth = Dimensions.get("window").width * 0.915;
 
 function ImageScroll(props) {
   return (
     <View>
-      {/* <PlaceholderImage opacity={props.opacity} /> */}
       <ScrollView
         style={{
           borderRadius: 15,
           opacity: props.opacity
-          // ,
-          // position: "absolute",
-          // left: 0,
-          // right: 0,
-          // bottom: 0,
-          // top: 0
         }}
         horizontal={true}
         decelerationRate={0}
-        snapToInterval={400} //your element width
+        snapToInterval={imageWidth} //your element width
         snapToAlignment={"center"}
       >
         {props.images.map((image, key) => {
+          preview = { uri: image.preview };
+          uri = { uri: image.uri };
           return (
             <Image
-              style={{ height: 300, width: 400, flex: 1 }}
+              style={{ height: 300, width: imageWidth, flex: 1 }}
               key={key}
-              {...image}
+              {...{ ...this.preview, ...this.uri }}
             />
           );
         })}
--- components/ItemImage/index.js
+++ components/ItemImage/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ImageScroll from "../ImageScroll";
-import PlaceholderImage from "../PlaceholderImage";
 import { Image } from "react-native-expo-image-cache";
 import { View } from "native-base";
+import { Dimensions } from "react-native";
+const imageWidth = Dimensions.get("window").width * 0.915;
 
 export default class ItemImage extends React.Component {
   preview = { uri: this.props.images[0].preview };
@@ -16,20 +17,14 @@ export default class ItemImage extends React.Component {
     } else {
       return (
         <View>
-          <PlaceholderImage opacity={this.props.opacity} />
           <Image
             style={{
               height: 300,
+              width: imageWidth,
               flex: 1,
               borderRadius: 15,
-              opacity: this.props.opacity,
-              position: "absolute",
-              left: 0,
-              right: 0,
-              bottom: 0,
-              top: 0
+              opacity: this.props.opacity
             }}
-            // {...this.props.images[0]}
             {...{ ...this.preview, ...this.uri }}
           />
         </View>
